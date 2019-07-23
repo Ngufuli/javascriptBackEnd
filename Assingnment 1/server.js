881b3ab413aaaac5330bd6b9c82e98cf76de15a9
Nicodemus Ngufuli <ngufuli@outlook.com>
2019-07-23 12:52:30 -0700

    Changes on the server

--- Assingnment 1/server.js
+++ Assingnment 1/server.js
@@ -6,6 +6,13 @@ const morgan = require('morgan');
 const computeObj = require('./computation/compute')
 
 const app = express();
+function notFound(req, res) {
+    const stream = fs.createReadStream(__dirname +
+        '/html/404.html', 'utf8');
+    res.status(404);
+    stream.pipe(res);
+}
+
 
 app.set('view engine', 'pug');
 app.use(morgan('tiny'));
@@ -13,39 +20,39 @@ app.use(bodyParser.urlencoded({
     extended: true,
     parameterLimit: 200
 }));
-app.use(expess.static('public'));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     const stream = fs.createReadStream(__dirname + 'html/index.html', 'utf8');
     stream.pipe(res);
 });
 
-if(req.url === '/add'){
+
     app.get('add/', function(res, req){
         res.render('sum.pug', {});
     });
     app.post('add/compute', computeObj.computeAdd);
-}
-else if(req.url === '/sub'){
+
+
     app.get('/sub', function(req, res){
         res.render('sub.pug', {});
     });
     app.post('sub/compute', computeObj.computeSub);
-}
-else if(req.url === '/mul'){
+
+
     app.get('mul', function(req, res){
         res.render('mul.pug', {});
     });
     app.post('mul/compute', computeObj.computeMul);
-}
-else if(req.url === '/div'){
+
+
     app.get('/mul', function(req, res){
         res.render('div.pug', computeObj.computeDiv);
     });
-}
-else{
-    app.post(res.send(404));
-}
+
+    app.use(notFound);
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
